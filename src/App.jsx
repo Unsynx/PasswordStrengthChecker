3c06d880d07ee6ac25750c117eb5f1ed078da06f
Nik <geeseflyomnidirectional@gmail.com>
2024-02-20 22:24:10 -0500

    Input with state

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,19 @@
+import { useState } from "react"
+
 function App() {
+  const [newPassword, setNewPassword] = useState("")
+
   return (
+    <>
     <h1 className="text-3xl font-bold underline">Hello, World</h1>
+    <form>
+      <input className="accent-black" 
+        type="text" 
+        value={newPassword}
+        onChange={e => setNewPassword(e.target.value)}
+      />
+    </form>
+    </>
   )
 }
 
